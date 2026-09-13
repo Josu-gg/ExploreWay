ba4c7cbb9c742af472b0584191e6338a87af10f6
Josu-gg <bp25001@esfe.agape.edu.sv>
2026-09-12 21:25:11 -0600

    Primer commit de ExploreWay

---
+++ src/main/java/org/esfe/ExploreWayApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreWayApplication.class, args);
+	}
+
+}
